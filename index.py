e72ff9b5348f7d06162a2e22191fda6cc25eed82
elle-chichi <mchellechi@gmail.com>
2024-10-30 18:49:41 +0300

    Add files via upload
    'First commit'

---
+++ index.py
@@ -0,0 +1,9 @@
+name = "Michelle Chichi"
+age = 16
+gender = "Female"
+
+print(name)
+
+languages = ["Java", "JavaScript", "Python", 67]
+print(languages)
+
---
+++ user_input.py
@@ -0,0 +1,6 @@
+name = "Michelle"
+age = 22
+location = "Paris"
+
+print(f"Hello {name}, you are {age}, and live in {location}")
+
