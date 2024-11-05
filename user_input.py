2b8b9578eccde5617fca7ee2264e0aef434e532c
elle-chichi <mchellechi@gmail.com>
2024-11-05 15:23:13 +0300

    Update user_input.py

--- user_input.py
+++ user_input.py
@@ -1,6 +1,5 @@
-name = "Michelle"
-age = 22
-location = "Paris"
+name = input("Please enter your name:")
+age = input("Please enter your age:")
+location = input("Please enter your location:")
 
 print(f"Hello {name}, you are {age}, and live in {location}")
-
